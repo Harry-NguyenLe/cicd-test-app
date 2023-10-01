595bcee39aee70186658f5ef4acc2b1a169f16f9
Calvin Nguyen Le Truong Hai <hainlt@fpt.com>
2023-10-01 23:31:22 +0700

    Multiple space added

--- app/src/main/java/com/example/cicd_test_app/MainActivity.kt
+++ app/src/main/java/com/example/cicd_test_app/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Hello to compose world")
+                    Greeting("Hello to compose world")          
                 }
             }
         }
